3b1215dd0900963d5825b0b28a965ae551f157d5
Nora Van Vleet <noravanvleet@gmail.com>
2023-05-24 14:17:11 -0500

    pulled from resume kata index and routing

---
+++ router.js
@@ -0,0 +1,46 @@
+const { Router } = require('express')
+
+const { seven } = require("./public/js/divideBy7Kata")
+const { reverse } = require("./public/js/reverseStringKata")
+const {watermelonDivider} = require("./public/js/watermelonKata");
+
+async function buildRouter() {
+    let router = new Router()
+
+    router.get('/', (req, res) => res.sendFile(`${__dirname}/index.html`))
+
+    router.get('/divideBySeven', (req, res) => {
+        let input = req.query.input
+        if(input) {
+            let result = seven(input, 0)
+            res.status(200).json({"result" : result})
+        } else {
+            res.status(400).json({error:"Please send input"})
+        }
+    })
+    router.get('/reverseString', (req, res) => {
+        let input = req.query.input
+        console.dir(req.query)
+        if(input) {
+            let result = reverse(input)
+            res.status(200).json({"result" : result})
+        } else {
+            res.status(400).json({error:"Please send input"})
+        }
+    })
+    router.get('/watermelon', (req, res) => {
+        let input = req.query.input
+        console.dir(req.query)
+        if(input) {
+            let result = watermelonDivider(input)
+            res.status(200).json({"result" : result})
+        } else {
+            res.status(400).json({error:"Please send input"})
+        }
+    })
+    return router
+}
+
+module.exports = {
+    buildRouter: buildRouter
+}
